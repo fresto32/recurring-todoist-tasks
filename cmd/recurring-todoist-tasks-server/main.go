041aa5fbb988f37c0f723a41a0516f131b315cac
Taj Pereira <tajpereira@gmail.com>
2021-10-16 10:44:11 +1030

    feat: get todoist API token from API_TOKEN environment variable

--- cmd/recurring-todoist-tasks-server/main.go
+++ cmd/recurring-todoist-tasks-server/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/fresto32/recurring-todoist-tasks/pkg/api"
 )
 
 func main() {
-	apiToken := "some token"
+	apiToken := os.Getenv("API_TOKEN")
 
 	projects := make(chan []api.Project)
 	go api.GetProjects(apiToken, projects)
